63efab04ffc2b597eec881933403d0d2666a60a3
136076063 <136076063@qq.com>
2019-04-02 22:49:49 +0800

    TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,21 @@
 import React, { Component, Fragment } from 'react'
 
 class TodoList extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            inputValue: '输入..',
+            listData: []
+        }
+    }
     render () {
         return (
-            <div>react</div>
+            <Fragment>
+                <div><input value={this.state.inputValue}/><button>提交</button></div>
+                <ul>
+                    <li>1</li>
+                </ul>
+            </Fragment>
         )
     };
 };
